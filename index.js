0cecbab294b257ea2cc6f16876079975a7dcd48d
laurijssen <lauserve@hotmail.com>
2023-02-11 14:35:36 +0100

    actually download kubectl

--- index.js
+++ index.js
@@ -3,12 +3,48 @@ const github = require('@actions/github');
 
 try {
   const version = core.getInput('kubectl-version');
-  console.log(`GET kubectl version ${version}`);
+
+  console.log(`Installing kubectl version ${version}`)
+  const kubectl = await downloadKubectl(version)
+
+  if (!kubectl) {
+      console.log(`kubectl not correctly downloaded version: ${version}`)
+      return
+  }
+
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  // const payload = JSON.stringify(github.context.payload, undefined, 2)
+  // console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
+}
+
+async function downloadKubectl(version) {
+	const url = `https://dl.k8s.io/release/${version}/bin/linux/amd64/kubectl`
+	const hashUrl = `${url}.sha256`
+
+	console.log(`Downloading kubectl (${url})`)
+	debug(`Downloading kubectl checksum (${hashUrl})`)
+
+	const hashResponse = await fetch(hashUrl)
+	if (!hashResponse.ok) {
+		debug(`Failed to download kubectl checksum with status ${hashResponse.status}`)
+		warning(`Skipping checksum verification for kubectl ${version}`)
+	}
+
+	const hash = hashResponse.ok ? await hashResponse.text() : ''
+
+	const response = await fetch(url)
+	if (!response.ok || !response.body) {
+		debug(`Failed to download kubectl with status ${response.status}`)
+		setFailed(`Failed to download kubectl with status ${response.status}`)
+		return
+	}
+
+	const hashStream = createHash('sha256')
+	const { body, headers } = response
+	const size = Number(headers.get('content-length'))
+	debug(`Downloaded kubectl (${size} bytes)`)
 }
